76c0dd345545a96ffdfff94aa53580c4095111cb
Kyle Kalwarski <kkalwarski@gmail.com>
2015-03-09 21:39:30 -0400

    bug fix

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-function setHash() {
     function setPageLoc(rel) {
         if (!rel) {
             return;
@@ -7,70 +6,56 @@ function setHash() {
         rel.addClass('active');
     }
 
-    var relPath = window.location.hash;
-    var $this = $('.navbar-nav li[data-slide="' + relPath + '"]');
-    setLoc($this);
-
-    $('.navbar-nav li a').click(function(e) {
-        var $this = $(this).parent();
-        if (window.location.pathname !== '/') {
-            var attr = $this.attr('data-slide');
-            window.location.href = window.location.origin + attr;
-        }
-
+    function setHash() {
+        var relPath = window.location.hash;
+        var $this = $('.navbar-nav li[data-slide="' + relPath + '"]');
         setPageLoc($this);
-        //e.preventDefault();
+    }
+    $(function() {
+        $('.navbar-nav li a').click(function(e) {
+            var $this = $(this).parent();
+            var attr = $this.attr('data-slide');
+            if (window.location.pathname !== '/') {
+                var attr = $this.attr('data-slide');
+                window.location.href = window.location.origin + attr;
+            } else {
+                $('html,body').animate({
+                    scrollTop: $(attr).offset().top
+                }, 1000);
+                //e.preventDefault();
+            }
+            setPageLoc($this);
+        });
+        $('.carousel').carousel({
+            interval: 2000
+        });
+        //setScroll();
+        //setHash();
+        addthis.layers({
+            'theme': 'transparent',
+            'share': {
+                'position': 'left',
+                'numPreferredServices': 5
+            },
+            'follow': {
+                'services': [{
+                    'service': 'twitter',
+                    'id': 'azimuth1tech'
+                }, {
+                    'service': 'linkedin',
+                    'id': 'azimuth1',
+                    'usertype': 'company'
+                }]
+            }
+        });
+        ! function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0],
+                p = /^http:/.test(d.location) ? 'http' : 'https';
+            if (!d.getElementById(id)) {
+                js = d.createElement(s);
+                js.id = id;
+                js.src = p + '://platform.twitter.com/widgets.js';
+                fjs.parentNode.insertBefore(js, fjs);
+            }
+        }(document, 'script', 'twitter-wjs');
     });
-}
-
-function setScroll() {
-    var links = $('.nav').find('li');
-    links.click(function(e) {
-        //e.preventDefault();
-        dataslide = $(this).attr('data-slide');
-
-        $('html, body').animate({
-            scrollTop: $(dataslide).offset().top
-        }, 500);
-
-    });
-}
-$(function() {
-
-    $('.carousel').carousel({
-        interval: 2000
-    });
-
-    setScroll();
-    setHash();
-
-    addthis.layers({
-        'theme': 'transparent',
-        'share': {
-            'position': 'left',
-            'numPreferredServices': 5
-        },
-        'follow': {
-            'services': [{
-                'service': 'twitter',
-                'id': 'azimuth1tech'
-            }, {
-                'service': 'linkedin',
-                'id': 'azimuth1',
-                'usertype': 'company'
-            }]
-        }
-    });
-
-    ! function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0],
-            p = /^http:/.test(d.location) ? 'http' : 'https';
-        if (!d.getElementById(id)) {
-            js = d.createElement(s);
-            js.id = id;
-            js.src = p + '://platform.twitter.com/widgets.js';
-            fjs.parentNode.insertBefore(js, fjs);
-        }
-    }(document, 'script', 'twitter-wjs');
-
-});
